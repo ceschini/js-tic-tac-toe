e4352f56b1126a0b96561711657f8ef748b60d5a
Lucas Ceschini <ceschini@protonmail.com>
2020-02-28 14:47:04 -0300

    button behavior added to tile 1x1

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 // drawing map
 ctx.lineWidth = 5;
-ctx.moveTo(200,0);
-ctx.lineTo(200,600);
+ctx.moveTo(200, 0);
+ctx.lineTo(200, 600);
 ctx.stroke();
-ctx.moveTo(400,0);
-ctx.lineTo(400,600);
+ctx.moveTo(400, 0);
+ctx.lineTo(400, 600);
 ctx.stroke();
-ctx.moveTo(0,200);
-ctx.lineTo(600,200);
+ctx.moveTo(0, 200);
+ctx.lineTo(600, 200);
 ctx.stroke();
-ctx.moveTo(0,400);
-ctx.lineTo(600,400);
+ctx.moveTo(0, 400);
+ctx.lineTo(600, 400);
 ctx.stroke();
-// drawing crosses
-// var crc = document.getElementById("cross");
-// var cross = crc.getContext("2d");
-// cross.beginPath();
-// cross.rect()
-// cross.f
+
+// button function
+function b1x1click() {
+  var btn = document.getElementById("b1x1");
+  btn.style.backgroundImage = "url('cross.png')";
+}
