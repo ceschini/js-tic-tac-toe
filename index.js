3b8c048411b7dc5770712753a2283a3c241074f7
ceschini <ceschini@protonmail.com>
2020-03-02 12:34:48 -0300

    sintax error fixed

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-// var canvas = document.getElementById("myCanvas");
-// var ctx = canvas.getContext("2d");
 var player = 1;
-// // drawing map
-// ctx.lineWidth = 5;
-// ctx.moveTo(200, 0);
-// ctx.lineTo(200, 600);
-// ctx.stroke();
-// ctx.moveTo(400, 0);
-// ctx.lineTo(400, 600);
-// ctx.stroke();
-// ctx.moveTo(0, 200);
-// ctx.lineTo(600, 200);
-// ctx.stroke();
-// ctx.moveTo(0, 400);
-// ctx.lineTo(600, 400);
-// ctx.stroke();
-
 // button function
 function btnclick(button) {
   var btn = document.getElementById(button);
